cb722897b4881330b360482524d2f99e76e2a0d0
Christian James Welly <chrisjwelly@gmail.com>
2019-05-19 21:38:07 +0800

    Add mouseover effect

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const container = document.querySelector(".container");
 // Creating the grids
 for (let i = 0; i < 16; i++) {
   const colGrid = document.createElement("div");
-  //rowGrid.textContent = "Col: " + i;
   for (let j = 0; j < 16; j++) {
     const rowGrid = document.createElement("div");
     rowGrid.classList.toggle("square");
-    rowGrid.textContent = "Row: " + j;
     colGrid.appendChild(rowGrid);
   }
   container.append(colGrid);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(sq => sq.addEventListener("mouseover", function(e) {
+    sq.setAttribute("style", "background: black");
+  })
+);
